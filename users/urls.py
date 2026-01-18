037a127b0e9a93f502c2cd0ed6a2da3179b94c1b
Shiva Doulagar <shivadoulager@gmail.com>
2026-01-18 23:06:19 +0530

    finally working

--- users/urls.py
+++ users/urls.py
@@ -1,32 +1,25 @@
 from django.urls import path
-from .import views
+from . import views
 from users.utility import train, test
 
 
-
-
 urlpatterns = [
-       path('registercheck/',views.registercheck,name='registercheck'),
-       path('userlogin',views.userlogin,name='userlogin'),
-       path('userlogincheck/',views.userlogincheck,name = 'userlogincheck'),
-       path('userlogout/',views.userlogout,name='userlogout'),
-       path('profile/', views.user_profile, name='userprofile'),
-       path('update_profile/',views.update_profile,name='update_profile'),
-       path('userhome/', views.user_home, name='userhome'),
-       path('Task1/',train.task1_view,name='Task1'),
-       # path('matrix', views.ConfusionMatrice, name='matrix'),
-       path('prediction', views.hate_speech_predictor, name='prediction'),
-       path('hate_speech_predictor_audio/', views.hate_speech_predictor_audio, name='hate_speech_predictor_audio'),
-
-       path('matrix/', train.confusion_matrix_view, name='matrix'),
-
-
-
-
-
-
-
-
+    path("registercheck/", views.registercheck, name="registercheck"),
+    path("userlogin", views.userlogin, name="userlogin"),
+    path("userlogincheck/", views.userlogincheck, name="userlogincheck"),
+    path("userlogout/", views.userlogout, name="userlogout"),
+    path("profile/", views.user_profile, name="userprofile"),
+    path("update_profile/", views.update_profile, name="update_profile"),
+    path("userhome/", views.user_home, name="userhome"),
+    path("Task1/", train.task1_view, name="Task1"),
+    # path('matrix', views.ConfusionMatrice, name='matrix'),
+    path("prediction", views.hate_speech_predictor, name="prediction"),
+    path(
+        "hate_speech_predictor_audio/",
+        views.hate_speech_predictor_audio,
+        name="hate_speech_predictor_audio",
+    ),
+    path("matrix/", train.confusion_matrix_view, name="matrix"),
 ]
 from django.conf import settings
 from django.conf.urls.static import static
